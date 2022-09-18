67c3b2207acbdc51bcf2110b7ee655f2767aea24
MatiStein <matistein@gmail.com>
2022-09-18 20:26:27 +0300

    # just another line

--- visitors/urls.py
+++ visitors/urls.py
@@ -3,10 +3,11 @@ from .views import (visitor_homepage, create_reservation, about,
                     ReservationList, ReservationView, RoomView)
 urlpatterns = [
     path('homepage', visitor_homepage, name='visitor_homepage'),
-    # a change
     path('create_reservation', create_reservation, name='create_reservation'),
     path('reservations', ReservationList.as_view(), name='reservations'),
     path('reservation-detail/<int:pk>', ReservationView.as_view(), name='reservation_detail'),
     path('room-detail.html/<int:pk>', RoomView.as_view(), name = 'room_detail' ),
     path('about/', about, name='about'),
 ]
+
+# just another line
